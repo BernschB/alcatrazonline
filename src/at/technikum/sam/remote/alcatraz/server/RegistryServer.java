730c00290cc6df981c11fff8695fdccfd110a104
juergen.zornig <juergen.zornig@gmail.com>
2011-03-11 06:58:49 +0000

    - Requirements Überbleibsel

---
+++ src/at/technikum/sam/remote/alcatraz/server/RegistryServer.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package at.technikum.sam.remote.alcatraz.server;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.Naming;
+import spread.AdvancedMessageListener;
+import spread.SpreadConnection;
+import spread.SpreadException;
+import spread.SpreadGroup;
+import spread.SpreadMessage;
+
+/**
+ *
+ * @author pinkerl
+ */
+public class RegistryServer implements AdvancedMessageListener {
+
+    private static String spreadHost = "localhost";
+
+    private SpreadConnection connection;
+    private SpreadGroup group;
+
+    public static void main (String args[]) {
+        new RegistryServer();
+    }
+
+    public RegistryServer() {
+        try {
+            System.out.println("Joining Spread Server Group...");
+            try
+            {
+                    /* Connect to local spread daemon and get spread group */
+                    connection = new SpreadConnection();
+                    connection.connect(InetAddress.getByName(spreadHost), 0, "AlcatrazRegistry", true, true);
+
+                    /* Add AdvancedMessageListener Implementation to this Spread connection */
+                    connection.add(this);
+            }
+            catch(SpreadException e)
+            {
+                    System.err.println("There was an error connecting to the daemon.");
+                    e.printStackTrace();
+                    System.exit(1);
+            }
+            catch(UnknownHostException e)
+            {
+                    System.err.println("Can't find the spread daemon on" + spreadHost);
+                    System.exit(1);
+            }
+
+            System.out.println("Starting up Registry Server instance...");
+            RegistryServerImplementation r = new RegistryServerImplementation();
+            System.out.println("Creating Registry...");
+            java.rmi.registry.LocateRegistry.createRegistry(1099);
+            Naming.rebind("rmi://localhost:1099/RegistryService", r);
+            System.out.println("Registry Server up and running.");
+        } catch (Exception e) {
+            System.out.println("Something went wrong while bringing up server.");
+            e.printStackTrace();
+        }
+    }
+
+    public void regularMessageReceived(SpreadMessage sm) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void membershipMessageReceived(SpreadMessage sm) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
