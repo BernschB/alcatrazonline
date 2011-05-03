232e8314ecec8cf19e8920c91bc45bb2cff27705
sebvogel@gmail.com <sebvogel@gmail.com@f0bb0722-7c72-6875-6f90-590d8162b4e9>
2011-05-03 19:56:02 +0000

    deleted directory Playground and doc

--- trunk/Playground/AlcatrazServerV4/server.cmd
+++
@@ -1,8 +0,0 @@
-
-REM ins arbeitsverzeichnis wechseln (anpassen!!)
-c:
-cd c:\eclipse\workspace\alcatrazserverv4
-
-REM starte server
-java -cp bin;spread-4.0.0.jar -Djava.rmi.server.codebase="file:/c:/eclipse/workspace/alcatrazserverv4/bin/vkm/server/server.class" -Djava.rmi.server.hostname=localhost -Djava.security.policy=server.policy vkm.server.Server
-
